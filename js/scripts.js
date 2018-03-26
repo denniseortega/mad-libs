6dd9c706c446509e7eaaa545f862d89c3e5a2f1a
Guest User <Guest@epicodus-10.local>
2018-03-26 10:30:23 -0700

    dry code

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,33 @@
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+//     var person1Input = $("input#person1").val();
+//     var person2Input = $("input#person2").val();
+//     var animalInput= $("input#animal").val();
+//     var colorInput = $("input#color").val();
+//     var verbInput = $("input#verb").val();
+//     var nounInput = $("input#noun").val();
+//
+//     $(".person1").text(person1Input);
+//     $(".person2").text(person2Input);
+//     $(".animal").text(animalInput);
+//     $(".color").text(colorInput);
+//     $(".verb").text(verbInput);
+//     $(".noun").text(nounInput);
+//
+//     $("#story").show();
+//
+//     event.preventDefault();
+//   });
+//});
+
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var colorInput = $("input#color").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var blanks = ["person1", "person2", "animal", "color", "verb", "noun"];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".color").text(colorInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
     $("#story").show();
 
